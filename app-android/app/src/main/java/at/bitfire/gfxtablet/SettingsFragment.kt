01e15387034d45b20e8d2e0a4655da946800994e
DafabHoid <github@dafabhoid.de>
2021-03-31 10:56:09 +0200

    Remove material design library
    This is not needed actually and makes the app a bit smaller

--- app-android/app/src/main/java/at/bitfire/gfxtablet/SettingsFragment.kt
+++ app-android/app/src/main/java/at/bitfire/gfxtablet/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package at.bitfire.gfxtablet
 
 import android.os.Bundle
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat()
@@ -8,5 +9,6 @@ class SettingsFragment : PreferenceFragmentCompat()
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?)
     {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        findPreference<EditTextPreference>(SettingsActivity.KEY_PREF_HOST)?.setOnBindEditTextListener { it.setSingleLine() }
     }
 }
